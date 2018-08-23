15053651be674b0c308449e9270ea3fe47adcbb9
Joe-Pena <joepena@protonmail.com>
2018-08-23 16:08:48 -0600

    FIXED IT, delete button and entry form now working

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 'use strict';
 
-const addItemToList = function(event) {
-  const searchText = $('.js-shopping-list-entry');
-  $('.shopping-list').append(`
-            <li>
-                <span class="shopping-item">${searchText.val()}</span>
-                <div class="shopping-items-controls">
-                    <button class="shopping-item-toggle">
-                        <span class="button-label">Check</span>
-                    </button>
-                    <button class="shopping-item-delete">
-                        <span class="button-label">Delete</span>
-                    </button>
-                </div>
-            </li>`);
-  searchText.val('');
-    
-};
+const addItem = function(entry) {
+    const htmlText = 
+        `<li>
+        <span class="shopping-item">${entry}</span>
+        <div class="shopping-items-controls">
+            <button class="shopping-item-toggle">
+                <span class="button-label">Check</span>
+            </button>
+            <button class="shopping-item-delete">
+                <span class="button-label">Delete</span>
+            </button>
+        </div>
+    </li>`
+    $('.shopping-list').append(htmlText);
+}
 
 
-
-
-
-$(
- 
-  $('form').on('submit', function(event) {
+$($('form').on('submit', function(event) {
     event.preventDefault();
-    const buttonTry = $('.js-shopping-list-entry').siblings(':submit');
-    buttonTry.click(addItemToList);
-  })
-
-);
+    const textEntry = $('.js-shopping-list-entry').val();
+    addItem(textEntry);
+    $('.js-shopping-list-entry').val('');
+    }
+));
 
 $(
   function(){
